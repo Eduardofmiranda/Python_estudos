c38279dda68aed3da0f13d07f6e60331d13757ab
Eduardo Miranda <miranda_eduardo.freitas@hotmail.com>
2024-04-16 16:58:04 -0300

    Try excpetion and modules py

--- Python_Udemy/aula93.py
+++ Python_Udemy/aula93.py
@@ -1,2 +1,19 @@
 # Try, except, else e finally
-teste
+#a = 18
+#b = 0
+#c = a / b    
+try:
+    a = 18
+    b = 0
+    print(b[0])
+    print('Linha 1')
+    c = a / b    
+except ZeroDivisionError:
+    print('Diviu por zero')
+except NameError:
+    print('Nome b não está definido')
+except (Exception) as error:
+    print('ERROR DESCONHECIDO', error.__class__.__name__)
+
+
+print('CONTINUAR')
