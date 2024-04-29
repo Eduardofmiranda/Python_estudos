f65bc880ddd48b37500e70ecf765a38087cd0ee3
Eduardo Miranda <miranda_eduardo.freitas@hotmail.com>
2024-04-29 16:31:39 -0300

    configurando ambiente virtual .venv e adicionando gitignore

--- Python_Udemy/aula114.py
+++ Python_Udemy/aula114.py
@@ -19,6 +19,13 @@ def recursiva(inicio=0, fim=4):
     # contar até chegar ao final
     inicio += 1
     return recursiva(inicio, fim)
+# print(recursiva())
 
+def factorial(n):
+    if n <= 1:
+        return 1
+    return n * factorial(n - 1)
 
-print(recursiva())
+print(factorial(5))
+print(factorial(10))
+
