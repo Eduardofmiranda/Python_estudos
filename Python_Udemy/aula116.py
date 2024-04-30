6e95e0453f9129f19cd3592de4c65426a81f2b7c
Eduardo Miranda <miranda_eduardo.freitas@hotmail.com>
2024-04-30 14:01:30 -0300

    '

--- Python_Udemy/aula116.py
+++ Python_Udemy/aula116.py
@@ -49,8 +49,9 @@ caminho_arquivo = 'aula116.txt'
 #     print(arquivo.read())
 
 with open(caminho_arquivo, 'w+') as arquivo:
-    arquivo.write('Linha 1\n')
-    arquivo.write('Linha 2\n')
-    arquivo.writelines(
-        ('Linha 3\n', 'Linha 4\n')
-    )
+    ...
+#     # arquivo.write('Linha 1\n')
+#     # arquivo.write('Linha 2\n')
+#     # arquivo.writelines(
+#     #     ('Linha 3\n', 'Linha 4\n')
+#     # )
