3e8d65e513e2a15c2b8086cbd50ddb03ffbe40a5
eduardofmiranda <miranda_eduardo.freitas@hotmail.com>
2024-09-05 16:44:02 -0300

    docStrings em class

--- Python_Udemy/aula156/main.py
+++ Python_Udemy/aula156/main.py
@@ -1,7 +1,7 @@
 # import uma_linha
-import varias_linhas
+import documentando_class
 
 # print(dir(uma_linha))
 # print(uma_linha.__file__)
 # print(uma_linha.__name__)
-help(varias_linhas)
+help(documentando_class)
